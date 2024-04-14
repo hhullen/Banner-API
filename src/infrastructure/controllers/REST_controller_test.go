272a7bc83793f76c9a79da477a49827bc0b214ff
Lipatov Maksim <hhullen@student.21-school.ru>
2024-04-14 22:15:09 +0700

    added: testing POST and PATHC methods

--- src/infrastructure/controllers/REST_controller_test.go
+++ src/infrastructure/controllers/REST_controller_test.go
@@ -3,9 +3,20 @@ package controllers_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	gen_api "main/go-server-generated/go"
 	"net/http"
 	"testing"
+)
+
+var (
+	client = &http.Client{}
+)
+
+const (
+	host       = "http://127.0.0.1:8080"
+	adminToken = "AVADAKEDABRA"
+	userToken  = "REDUKTO"
 )
 
 var testBanner1 = gen_api.BannerIdBody{
@@ -52,9 +63,115 @@ var testBanner4 = gen_api.BannerIdBody{
 	IsActive: false,
 }
 
-func TestAddActor(t *testing.T) {
-	data, _ := json.Marshal(testBanner1)
-	req, _ := http.NewRequest("POST", "localhost:8080/banner", bytes.NewBuffer(data))
+func TestUnauthorizedRequest(t *testing.T) {
+	req, _ := http.NewRequest("POST", host+"/banner", nil)
 
-	req.Header.Set("user_token", "TESTING_TOKEN_")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatal("Status code:", resp.Status)
+	}
+}
+
+func TestPostForbiddenUser(t *testing.T) {
+	data, _ := json.Marshal(testBanner1)
+	req, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data))
+
+	req.Header.Set("user_token", userToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusForbidden {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusForbidden)
+	}
+}
+
+func TestPostBanners(t *testing.T) {
+	data1, _ := json.Marshal(testBanner1)
+	req1, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data1))
+	data2, _ := json.Marshal(testBanner1)
+	req2, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data2))
+	data3, _ := json.Marshal(testBanner1)
+	req3, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data3))
+	data4, _ := json.Marshal(testBanner1)
+	req4, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data4))
+
+	req1.Header.Set("user_token", adminToken)
+	req1.Header.Set("Content-Type", "application/json")
+	req2.Header.Set("user_token", adminToken)
+	req2.Header.Set("Content-Type", "application/json")
+	req3.Header.Set("user_token", adminToken)
+	req3.Header.Set("Content-Type", "application/json")
+	req4.Header.Set("user_token", adminToken)
+	req4.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+	resp, err = client.Do(req2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+	resp, err = client.Do(req3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+	resp, err = client.Do(req4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+}
+
+func TestPatchBanner(t *testing.T) {
+	patchedBanner := gen_api.BannerIdBody{
+		TagIds:    []int32{1, 2, 3},
+		FeatureId: 1,
+		Content: gen_api.ModelMap{
+			Title: "PATCHED",
+			Text:  "PATCHED",
+			Url:   "http://PATCHED.com",
+		},
+		IsActive: true,
+	}
+	data, _ := json.Marshal(patchedBanner)
+	var resp *http.Response
+	for i := range 30 {
+		req, err := http.NewRequest("PATCH", host+"/banner/"+fmt.Sprint(i), bytes.NewBuffer(data))
+		req.Header.Set("user_token", adminToken)
+
+		req.Header.Set("Content-Type", "application/json")
+		resp, err = client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			break
+		}
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
 }
