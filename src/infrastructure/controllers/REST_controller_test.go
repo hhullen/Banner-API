040f1995cfd3a8be2370ecb2bbacdb564d51fc81
Lipatov Maksim <hhullen@student.21-school.ru>
2024-04-14 23:40:39 +0700

    added: tests for GET and DELETE methods

--- src/infrastructure/controllers/REST_controller_test.go
+++ src/infrastructure/controllers/REST_controller_test.go
@@ -94,14 +94,16 @@ func TestPostForbiddenUser(t *testing.T) {
 	}
 }
 
+var existingID = "1"
+
 func TestPostBanners(t *testing.T) {
 	data1, _ := json.Marshal(testBanner1)
 	req1, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data1))
-	data2, _ := json.Marshal(testBanner1)
+	data2, _ := json.Marshal(testBanner2)
 	req2, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data2))
-	data3, _ := json.Marshal(testBanner1)
+	data3, _ := json.Marshal(testBanner3)
 	req3, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data3))
-	data4, _ := json.Marshal(testBanner1)
+	data4, _ := json.Marshal(testBanner4)
 	req4, _ := http.NewRequest("POST", host+"/banner", bytes.NewBuffer(data4))
 
 	req1.Header.Set("user_token", adminToken)
@@ -120,6 +122,11 @@ func TestPostBanners(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
 	}
+
+	var resp_struct gen_api.InlineResponse201
+	json.NewDecoder(resp.Body).Decode(&resp_struct)
+	existingID = fmt.Sprint(resp_struct.BannerId)
+
 	resp, err = client.Do(req2)
 	if err != nil {
 		t.Fatal(err)
@@ -156,19 +163,84 @@ func TestPatchBanner(t *testing.T) {
 	}
 	data, _ := json.Marshal(patchedBanner)
 	var resp *http.Response
-	for i := range 30 {
-		req, err := http.NewRequest("PATCH", host+"/banner/"+fmt.Sprint(i), bytes.NewBuffer(data))
-		req.Header.Set("user_token", adminToken)
 
-		req.Header.Set("Content-Type", "application/json")
-		resp, err = client.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
+	req, err := http.NewRequest("PATCH", host+"/banner/"+existingID, bytes.NewBuffer(data))
+	req.Header.Set("user_token", adminToken)
 
-		if resp.StatusCode == http.StatusOK {
-			break
-		}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+}
+
+func TestGetBanners(t *testing.T) {
+	req, _ := http.NewRequest("GET", host+"/banner?feature_id=2&tag_id=4&limit=5&offset=0", nil)
+	req.Header.Set("user_token", adminToken)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+}
+
+func TestGetInnactiveBanners(t *testing.T) {
+	req, _ := http.NewRequest("GET", host+"/banner?feature_id=4&tag_id=9&limit=5&offset=0", nil)
+	req.Header.Set("user_token", userToken)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusNotFound {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusNotFound)
+	}
+}
+
+func TestGetUserBanner(t *testing.T) {
+	req1, _ := http.NewRequest("GET", host+"/user_banner?feature_id=3&tag_id=3&use_last_revision=true", nil)
+	req2, _ := http.NewRequest("GET", host+"/user_banner?feature_id=3&tag_id=3&use_last_revision=false", nil)
+	req1.Header.Set("user_token", userToken)
+	req1.Header.Set("Content-Type", "application/json")
+	req2.Header.Set("user_token", userToken)
+	req2.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+
+	resp, err = client.Do(req2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Status code:", resp.Status, "Expected: ", http.StatusOK)
+	}
+}
+
+func TestDeleteBanner(t *testing.T) {
+	req, _ := http.NewRequest("DELETE", host+"/banner/"+existingID, nil)
+	req.Header.Set("user_token", adminToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
